7fc5f39379fdf05f66edd4656ca5d25b3bd95c0f
Julian Solorzano <yayforme789@gmail.com>
2019-11-11 18:41:53 -0600

    initial commit

---
+++ __manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Reporte venta',
+    'category': '',
+    'summary': '',
+    'author': 'JSS',
+    'website': '',
+    'version': '1.0',
+    'description':"""
+Reporte venta tipo ticket
+""",
+    'images': [],
+    'depends': [],
+    'data': [
+        'report.xml',
+        'report_paperformat.xml',
+        'report_receipt.xml'
+    ],
+    'installable': True,
+    'auto_install': False,
+}
